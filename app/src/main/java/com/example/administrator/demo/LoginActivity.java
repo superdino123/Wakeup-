132530d39cc7aaf1a9da3a9d8c2f9b7e6af3b5b5
fitx <897920245@qq.com>
2016-12-27 19:00:40 +0800

    firstcommit wakeup!

---
+++ app/src/main/java/com/example/administrator/demo/LoginActivity.java
@@ -0,0 +1,238 @@
+package com.example.administrator.demo;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+
+import common.UserData;
+import common.UserDataManager;
+import common.Utils;
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class LoginActivity extends AppCompatActivity  {
+
+    //头像处理
+    protected static final int CHOOSE_PICTURE = 0;
+    protected static final int TAKE_PICTURE = 1;
+    private static final int CROP_SMALL_PICTURE = 2;
+    protected static Uri tempUri;
+    private ImageView iv_personal_icon;  //头像图片
+
+    private EditText mAccount;                        //用户名编辑
+    private EditText mPwd;                            //密码编辑
+    private EditText mPwdCheck;                       //密码编辑
+    private Button mSureButton;                       //确定按钮
+    private UserDataManager mUserDataManager;         //用户数据管理类
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        //应该检查是否注册过
+
+        //头像选择
+        iv_personal_icon = (ImageView) findViewById(R.id.iv_personal_icon);
+        iv_personal_icon.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showChoosePicDialog();
+            }
+        });
+
+        //信息录入
+        mAccount = (EditText) findViewById(R.id.resetpwd_edit_name);
+        mPwd = (EditText) findViewById(R.id.resetpwd_edit_pwd_old);
+        mPwdCheck = (EditText) findViewById(R.id.resetpwd_edit_pwd_new);
+
+        mSureButton = (Button) findViewById(R.id.register_btn_sure);
+
+        mSureButton.setOnClickListener(m_register_Listener);      //注册界面两个按钮的监听事件
+
+
+        if (mUserDataManager == null) {
+            mUserDataManager = new UserDataManager(this);
+            mUserDataManager.openDataBase();                              //建立本地数据库
+
+        }
+
+    }
+
+    /**
+     * 显示修改头像的对话框
+     */
+    protected void showChoosePicDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("设置头像");
+        String[] items = { "选择本地照片", "拍照" };
+        builder.setNegativeButton("取消", null);
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+                    case CHOOSE_PICTURE: // 选择本地照片
+                        Intent openAlbumIntent = new Intent(
+                                Intent.ACTION_GET_CONTENT);
+                        openAlbumIntent.setType("image/*");
+                        startActivityForResult(openAlbumIntent, CHOOSE_PICTURE);
+                        break;
+                    case TAKE_PICTURE: // 拍照
+                        Intent openCameraIntent = new Intent(
+                                MediaStore.ACTION_IMAGE_CAPTURE);
+                        tempUri = Uri.fromFile(new File(Environment
+                                .getExternalStorageDirectory(), "image.jpg"));
+                        // 指定照片保存路径（SD卡），image.jpg为一个临时文件，每次拍照后这个图片都会被替换
+                        openCameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, tempUri);
+                        startActivityForResult(openCameraIntent, TAKE_PICTURE);
+                        break;
+                }
+            }
+        });
+        builder.create().show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) { // 如果返回码是可以用的
+            switch (requestCode) {
+                case TAKE_PICTURE:
+                    startPhotoZoom(tempUri); // 开始对图片进行裁剪处理
+                    break;
+                case CHOOSE_PICTURE:
+                    startPhotoZoom(data.getData()); // 开始对图片进行裁剪处理
+                    break;
+                case CROP_SMALL_PICTURE:
+                    if (data != null) {
+                        setImageToView(data); // 让刚才选择裁剪得到的图片显示在界面上
+                    }
+                    break;
+            }
+        }
+    }
+
+    /**
+     * 裁剪图片方法实现
+     *
+     * @param uri
+     */
+    protected void startPhotoZoom(Uri uri) {
+        if (uri == null) {
+            Log.i("tag", "The uri is not exist.");
+        }
+        tempUri = uri;
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        intent.setDataAndType(uri, "image/*");
+        // 设置裁剪
+        intent.putExtra("crop", "true");
+        // aspectX aspectY 是宽高的比例
+        intent.putExtra("aspectX", 1);
+        intent.putExtra("aspectY", 1);
+        // outputX outputY 是裁剪图片宽高
+        intent.putExtra("outputX", 150);
+        intent.putExtra("outputY", 150);
+        intent.putExtra("return-data", true);
+        startActivityForResult(intent, CROP_SMALL_PICTURE);
+    }
+
+    /**
+     * 保存裁剪之后的图片数据
+     *
+     */
+    protected void setImageToView(Intent data) {
+        Bundle extras = data.getExtras();
+        if (extras != null) {
+            Bitmap photo = extras.getParcelable("data");
+            photo = Utils.toRoundBitmap(photo, tempUri); // 这个时候的图片已经被处理成圆形的了
+            iv_personal_icon.setImageBitmap(photo);
+            uploadPic(photo);
+        }
+    }
+
+    private void uploadPic(Bitmap bitmap) {
+
+        String imagePath = Utils.savePhoto(bitmap, Environment
+                .getExternalStorageDirectory().getAbsolutePath(), String
+                .valueOf(System.currentTimeMillis()));
+        Log.e("imagePath", imagePath+"");
+    }
+
+
+
+    View.OnClickListener m_register_Listener = new View.OnClickListener() {    //不同按钮按下的监听事件选择
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.register_btn_sure:                       //确认按钮的监听事件
+                    register_check();
+                    break;
+            }
+        }
+    };
+
+    public void register_check() {                                //确认按钮的监听事件
+        if (isUserNameAndPwdValid()) {
+            String userName = mAccount.getText().toString().trim();
+            String userPwd = mPwd.getText().toString().trim();
+            String userPwdCheck = mPwdCheck.getText().toString().trim();
+            //检查用户是否存在
+            int count=mUserDataManager.findUserByName(userName);
+            //用户已经存在时返回，给出提示文字
+            if(count>0){
+                Toast.makeText(this, getString(R.string.name_already_exist, userName),Toast.LENGTH_SHORT).show();
+                return ;
+            }
+            if(userPwd.equals(userPwdCheck)==false){     //两次密码输入不一样
+                Toast.makeText(this, getString(R.string.pwd_not_the_same),Toast.LENGTH_SHORT).show();
+                return ;
+            } else {
+                UserData mUser = new UserData(userName, userPwd);
+                mUserDataManager.openDataBase();
+                long flag = mUserDataManager.insertUserData(mUser); //新建用户信息
+                if (flag == -1) {
+                    Toast.makeText(this, getString(R.string.register_fail),Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(this, getString(R.string.register_success),Toast.LENGTH_SHORT).show();
+                    //跳转到用户界面
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        }
+    }
+    public boolean isUserNameAndPwdValid() {
+        if (mAccount.getText().toString().trim().equals("")) {
+            Toast.makeText(this, getString(R.string.account_empty),
+                    Toast.LENGTH_SHORT).show();
+            return false;
+        } else if (mPwd.getText().toString().trim().equals("")) {
+            Toast.makeText(this, getString(R.string.pwd_empty),
+                    Toast.LENGTH_SHORT).show();
+            return false;
+        }else if(mPwdCheck.getText().toString().trim().equals("")) {
+            Toast.makeText(this, getString(R.string.pwd_check_empty),
+                    Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
+
+}
+
